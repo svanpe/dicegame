9079af180a85754fa76be9d1c06c36804d691608
ashmi.grace <ashmigrace85@gmail.com>
2021-01-11 18:12:40 +0100

    Step definitions added and tested

--- src/test/java/stepdefs/StepDefinition.java
+++ src/test/java/stepdefs/StepDefinition.java
@@ -1,28 +1,37 @@
 package stepdefs;
 
-import io.cucumber.java.PendingException;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import be.svanpe.dicegame.game.Dice;
+import be.svanpe.dicegame.game.SameFigureCardRule;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinition {
 
+    private final SameFigureCardRule cardRule = new SameFigureCardRule(Dice.Figures.six, 3);
+    private final List<Dice> diceList = new ArrayList<>();
+
+
     @Given("^the player rolls six dices$")
-    public void the_player_rolls_six_dices() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    public void the_player_rolls_six_dices() {
         System.out.println("dice is thrown");
     }
 
-    @When("^the (\\d+) dice shows number (\\d+) with \"([^\"]*)\" figure$")
-    public void the_dice_shows_number_with_figure(int arg1, int arg2, String arg3) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("dice combinations");
+    @When("^the (\\d+) dice shows number \"([^\"]*)\" with \"([^\"]*)\" figure$")
+    public void the_dice_shows_number_with_figure(int arg1, String arg2, String arg3) {
+        Dice dice = new Dice(Dice.Colors.valueOf(arg3), Dice.Figures.valueOf(arg2));
+        diceList.add(dice);
+
     }
 
     @Then("^the same figure rule is \"([^\"]*)\"$")
-    public void the_same_figure_rule_is(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("result");
+    public void the_same_figure_rule_is(String arg1) {
+        assertEquals(Boolean.parseBoolean(arg1), cardRule.isValid(diceList));
     }
 
 }
